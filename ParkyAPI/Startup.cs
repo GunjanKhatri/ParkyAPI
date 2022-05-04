2ed9d8e791a051822911e03870664a9d0970000c
Gunjan Advani <Gunjan.Advani@shell.com>
2022-05-04 10:40:41 +0530

    Add project files.

---
+++ ParkyAPI/Startup.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ParkyAPI.Data;
+using ParkyAPI.ParkyMapper;
+using ParkyAPI.Repository;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
+            services.AddScoped<ITrailRepository, TrailRepository>();
+            services.AddAutoMapper(typeof(ParkyMappings));
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("ParkyOpenAPISpec NP",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Parky API NP",
+                        Version = "1",
+                        Description = "Udemy Parky API NP",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                        {
+                            Email = "bhrugen.udemy@gmail.com",
+                            Name = "Bhrugen Patel",
+                            Url = new Uri("https://wwww.bhrugen.com")
+                        },
+                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                        {
+                            Name = "MIT License",
+                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                        }
+
+                    });
+
+                options.SwaggerDoc("ParkyOpenAPISpecTrails",
+                   new Microsoft.OpenApi.Models.OpenApiInfo()
+                   {
+                       Title = "Parky API Trails",
+                       Version = "1",
+                       Description = "Udemy Parky API Trails",
+                       Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                       {
+                           Email = "bhrugen.udemy@gmail.com",
+                           Name = "Bhrugen Patel",
+                           Url = new Uri("https://wwww.bhrugen.com")
+                       },
+                       License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                       {
+                           Name = "MIT License",
+                           Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                       }
+
+                   });
+                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+                options.IncludeXmlComments(cmlCommentsFullPath);
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(options=> {
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
+                options.RoutePrefix= "";
+            });
+            app.UseMvc();
+        }
+    }
+}
